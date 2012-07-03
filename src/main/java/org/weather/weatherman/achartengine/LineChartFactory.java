40405e7b4b97b2adc4407b122f20f58eefe14648
gengmzh <gengmzh@gmail.com>
2012-07-03 11:27:59 +0800

    add weather trend

---
+++ src/main/java/org/weather/weatherman/achartengine/LineChartFactory.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package org.weather.weatherman.achartengine;
+
+import org.achartengine.GraphicalView;
+import org.achartengine.chart.XYChart;
+import org.achartengine.model.XYMultipleSeriesDataset;
+import org.achartengine.renderer.XYMultipleSeriesRenderer;
+
+import android.content.Context;
+
+/**
+ * @author gmz
+ * 
+ */
+public class LineChartFactory {
+
+	public static final GraphicalView getLineChartView(Context context, XYMultipleSeriesDataset dataset,
+			XYMultipleSeriesRenderer renderer) {
+		checkParameters(dataset, renderer);
+		XYChart chart = new MyLineChart(dataset, renderer, 10f);
+		return new GraphicalView(context, chart);
+	}
+
+	private static void checkParameters(XYMultipleSeriesDataset dataset, XYMultipleSeriesRenderer renderer) {
+		if (dataset == null || renderer == null || dataset.getSeriesCount() != renderer.getSeriesRendererCount()) {
+			throw new IllegalArgumentException(
+					"Dataset and renderer should be not null and should have the same number of series");
+		}
+	}
+
+}
---
+++ src/main/java/org/weather/weatherman/achartengine/MyXYSeries.java
@@ -0,0 +1,51 @@
+/**
+ * 
+ */
+package org.weather.weatherman.achartengine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.achartengine.model.XYSeries;
+
+/**
+ * @author gmz
+ * 
+ */
+public class MyXYSeries extends XYSeries {
+
+	private static final long serialVersionUID = 2516000745378907666L;
+	private static final String seperator = "#";
+
+	private Map<String, String> labels = new HashMap<String, String>();
+
+	public MyXYSeries(String title) {
+		super(title);
+	}
+
+	public MyXYSeries(String title, int scaleNumber) {
+		super(title, scaleNumber);
+	}
+
+	public synchronized void add(double x, double y, String label) {
+		add(x, y);
+		labels.put(normalize(x) + seperator + normalize(y), label);
+	}
+
+	public String getLabel(double x, double y) {
+		String label = labels.get(normalize(x) + seperator + normalize(y));
+		if (label == null) {
+			label = normalize(y);
+		}
+		return label;
+	}
+
+	String normalize(double value) {
+		if (value == Math.round(value)) {
+			return String.valueOf(Math.round(value));
+		} else {
+			return String.valueOf(value);
+		}
+	}
+
+}
